8abc792d38fa0c4cc8e65254064f8f02b42322d6
Rus <de.mr44@gmail.com>
2022-01-31 20:02:10 +0500

    Added UI

--- Patterns-Observer/ViewController.swift
+++ Patterns-Observer/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func updateAction(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func switchAction(_ sender: UISwitch) {
+        
+    }
 }
 
